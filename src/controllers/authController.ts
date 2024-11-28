9c5a3be9a91733cf4006e8d00721bc15eff08557
i-am-abdullah <heyiamabdullah@gmail.com>
2024-11-29 00:14:54 +0500

    made a controller for register user

---
+++ src/controllers/authController.ts
@@ -0,0 +1,37 @@
+import { Request, Response, RequestHandler } from 'express';
+import bcrypt from 'bcrypt';
+import { User } from '../models/User';
+
+export const registerUser: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { fullName, username, email, password } = req.body;
+  
+      const existingUser = await User.findOne({
+        $or: [{ username }, { email }],
+      });
+  
+      if (existingUser) {
+        res.status(400).json({ message: 'Username or email already exists' });
+        return;
+      }
+  
+      const newUser = new User({
+        fullName,
+        username,
+        email,
+        password,
+      });
+  
+      const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT_ROUNDS));
+      newUser.password = hashedPassword;
+      await newUser.save();
+  
+      res.status(201).json({
+        message: 'User registered successfully',
+        user: { fullName, username, email },
+      });
+    } catch (error) {
+      console.error('Error registering user:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
